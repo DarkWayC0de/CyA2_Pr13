f28d4e521a7b216441b377df4942675014076c73
Diego_Cruz_Rodriguez <alu0101105802@ull.edu.es>
2020-01-04 02:13:46 +0000

    Codígo proporcionado enel enunciado de la Practica

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include <cstdio>
+#include <vector>
+#include <random>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+#include "sorted_vector_t.hpp"
+
+#define LIMIT 999
+#define SIZE   80
+
+using namespace std;
+
+int main(void)
+{
+  default_random_engine generator;
+  uniform_int_distribution<int> distribution(-LIMIT, LIMIT);
+
+  CyA::sorted_vector_t v;
+
+  for(int i = 0; i < SIZE; i++)
+    v.push_back(distribution(generator));
+
+  cout << v << endl;
+
+  v.merge_sort();
+
+  cout << v << endl;
+
   return 0;
 }
